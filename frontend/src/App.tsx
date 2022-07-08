c350358552da6b5e5a675397bf65a71b1a9b072a
Rowan Richardson <rr.designweb@gmail.com>
2022-07-09 08:36:54 +1000

    add frontend folder contents

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+          Entry
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
